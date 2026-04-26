ff4cfd383b91ca724391a7519b6fd2ccc5ed1410
AG04 <agocs.gergely02@gmail.com>
2026-04-26 19:09:37 +0200

    Stopszó filter elkészítve.

---
+++ word_lists/stopszo_filter.py
@@ -0,0 +1,4 @@
+stopszavak = set(open("stopwords_hu.txt", encoding="utf-8").read().splitlines())
+
+for szo in open("top_freq_5000_hu.txt", encoding="utf-8").read().splitlines():
+    if szo in stopszavak: print(szo)
